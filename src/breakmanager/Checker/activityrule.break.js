627dbb87c7a629197783a59837dccd1d2fc52053
Ajinkya Deshpande <ajdesh2000@gmail.com>
2021-01-06 22:57:05 +0530

    triggers working

--- src/breakmanager/Checker/activityrule.break.js
+++ src/breakmanager/Checker/activityrule.break.js
@@ -7,7 +7,6 @@ let client = new AWClientService()
 export default function checkActivityRuleBreak()
 {
     client.getCurrentlyActiveWindow().then(ob => {
-        console.log(ob)
         if ((ob.duration > 20) && ob.data.title.includes("Facebook") && !(store.getState().break.breakState === "break")) {
             store.dispatch(BreakActions.startBreak())
         }
