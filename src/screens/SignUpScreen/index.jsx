f1711b54afdd84071f6d5f57cc0893a11f56b1dc
Shriram <shriram@normalyze.ai>
2021-11-01 20:29:01 +0530

    stuff

--- src/screens/SignUpScreen/index.jsx
+++ src/screens/SignUpScreen/index.jsx
@@ -33,6 +33,7 @@ const SignupScreen = (props) => {
       education,
       location,
       goal,
+      type: "2",
     };
     let response = await axios.post(
       "https://thepallab.com/api/user/register",
